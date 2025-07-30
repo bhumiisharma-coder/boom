b0e3da7699ee37e388e2254de220161b8ec38090
bhumiisharma-coder <bhumiisharma.2007@gmail.com>
2025-07-30 13:36:10 +0530

    Add files via upload

---
+++ model.js
@@ -0,0 +1,15 @@
+// models/Submission.js
+const mongoose = require('mongoose');
+
+const submissionSchema = new mongoose.Schema({
+  fullName: String,
+  email: String,
+  inquiryType: String,
+  message: String,
+  submittedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Submission', submissionSchema);
